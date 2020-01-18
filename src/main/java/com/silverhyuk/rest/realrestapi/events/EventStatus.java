4ef7d510fba03d9bb38bffb38a91fa4c02846245
silverhyuk84 <silverhyuk84@gmail.com>
2020-01-18 23:00:26 +0900

    created event domain

---
+++ src/main/java/com/silverhyuk/rest/realrestapi/events/EventStatus.java
@@ -0,0 +1,5 @@
+package com.silverhyuk.rest.realrestapi.events;
+
+public enum EventStatus {
+    DRAFT, PUBLISHED, BEGAN_ENROLLMEND, CLOSED_ENROLLMENT, STARTED, ENDED
+}
---
+++ src/test/java/com/silverhyuk/rest/realrestapi/events/EventTest.java
@@ -0,0 +1,34 @@
+package com.silverhyuk.rest.realrestapi.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Spring Rest API")
+                .description("Rest API With Spring boot")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        //Given
+        String name = "Event";
+        String description = "Spring";
+
+        //When
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+
+    }
+}
