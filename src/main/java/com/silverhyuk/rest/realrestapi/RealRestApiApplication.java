118c28c413519fa74dd5c22395d2a58d65ecb8fb
silverhyuk84 <silverhyuk84@gmail.com>
2020-01-18 22:41:20 +0900

    Initial commit

---
+++ src/main/java/com/silverhyuk/rest/realrestapi/RealRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.silverhyuk.rest.realrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silverhyuk/rest/realrestapi/RealRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silverhyuk.rest.realrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
